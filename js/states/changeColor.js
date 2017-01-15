557d7acd3e8078d276a6164441978e145c470b22
Evan Cobb <cobb.evan@gmail.com>
2017-01-15 17:12:50 -0600

    one scene can hold multiple shaders now

--- js/states/changeColor.js
+++ js/states/changeColor.js
@@ -1,8 +1,15 @@
 var changeColor = function(game) {
     return {
+        shaderKeys: ['colorchange', 'second'],
+        currentShader: -1,
+
         preload: function(game) {
             game.load.image('berry', 'assets/berry.png');
-            game.load.shader('colorchange', 'shaders/colorchange.frag');
+
+            for (var i = 0; i < this.shaderKeys.length; ++i) {
+                var key = this.shaderKeys[i];
+                game.load.shader(key, 'shaders/' + key + '.frag');
+            }
         },
 
         create: function(game) {
@@ -10,14 +17,20 @@ var changeColor = function(game) {
             this.sprite.width = 300;
             this.sprite.height = 300;
             this.graphics = game.add.graphics(0, 0);
-
-            filter = new Phaser.Filter(game, null, game.cache.getShader('colorchange'));
-
-            this.sprite.shader = filter;
+            this.setNextShader(game);
+            this.game.input.onDown.add(function() {
+                this.setNextShader(game);
+            }, this);
         },
 
         render: function() {
             this.graphics.clear();
+        },
+
+        setNextShader: function(game) {
+            var index = ++this.currentShader % this.shaderKeys.length;
+            var filter = new Phaser.Filter(game, null, game.cache.getShader(this.shaderKeys[index]));
+            this.sprite.shader = filter;
         }
     };
 };
